90fff0cc895fef3611d4d7c9efe31d34f9d8bdb8
Ryan Aclan <77793451+raclan@users.noreply.github.com>
2023-04-20 17:53:45 +0000

    modify files

--- components/Graph1.js
+++ components/Graph1.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-import loveActually from  '../datas/love-actually.json';
+// import shootingCauses from  '../datas/graph1/shooting-causes.csv';
 
 const Graph1 = () => {
+
   const svgRef = useRef(null);
 
+  d3.csv('/workspaces/vistualizations-school-shooting/datas/graph1/shooting-causes.csv').then(data=>{
+    console.log(data);
+  });
+
   useEffect(() => {
+
+
+    d3.csv('/workspaces/vistualizations-school-shooting/datas/graph1/shooting-causes.csv').then(data=>{
+      console.log(data);
+    });
+
     const width = 1200;
     const height = 800;
 
@@ -13,37 +24,12 @@ const Graph1 = () => {
       .attr('width', width)
       .attr('height', height);
 
-    const circle = svg.selectAll('circle')
-      .data(loveActually.nodes)
-      .enter()
-      .append('circle')
-      .attr('r', 10)
-      .style('fill', 'green')
-      .style('opacity', 0.5);
 
-    const line = svg.selectAll('line')
-      .data(loveActually.links)
-      .join('line')
-      .style('stroke', 'gray')
-      .style('opacity', 0.5)
-      .attr('stroke-width', '1px');
+  
+    // var line = d3.line()
+    //   .x(function(d) { return x(shootingCauses.Year); })
+    //   .y(function(d) {return y(+shootingCauses.); })
 
-    const simulation = d3.forceSimulation(loveActually.nodes)
-      .force('charge', d3.forceManyBody().strength(-25))
-      .force('center', d3.forceCenter(width / 2, height / 2))
-      .force('link', d3.forceLink(loveActually.links).id(d => d.id))
-      .force('collision', d3.forceCollide().radius(15))
-      .on('tick', ticked);
-
-    function ticked() {
-      circle.attr('cx', d => d.x)
-        .attr('cy', d => d.y);
-
-      line.attr('x1', d => d.source.x)
-        .attr('y1', d => d.source.y)
-        .attr('x2', d => d.target.x)
-        .attr('y2', d => d.target.y);
-    }
   }, []);
 
   return (
